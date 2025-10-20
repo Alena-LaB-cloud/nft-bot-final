2a5118a4b8ab60cefc38812c1a0c620072616b7c
Alena-LaB-cloud <alenaspiridonova2@mail.ru>
2025-10-20 15:19:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import os
+
+import logger
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return "Hello World from Flask!"
 
+
 @app.route('/health')
 def health():
     return "OK"
+
 
 port = int(os.environ.get("PORT", 10000))
 if __name__ == "__main__":
@@ -17,5 +22,4 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
     logger.info(f"🚀 Starting web server on port {port}")
     app.run(host='0.0.0.0', port=port, debug=False)
-    
 
